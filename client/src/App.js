e81bc03b59a3b74dd132aff328dc4294208166f7
Elliott Jones <elliottohj44@gmail.com>
2019-09-30 18:19:45 -0400

    trivial commit v3

--- client/src/App.js
+++ client/src/App.js
@@ -116,7 +116,32 @@ class App extends Component {
     this.spellsDB.on('value', snapshot => {
       this.setState({ spells: snapshot.val() });
     });
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
   }
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+  
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+    
+    this.setState({ responseToPost: body });
+  };
+  
   onOpenChatPanel(e) {
     e.preventDefault();
     if (this.state.chatOpen === false) {
