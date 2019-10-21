9d19589d9da0721313e5df08d9305e96d0743294
Elliott Jones <elliottohj44@gmail.com>
2019-10-21 01:19:02 -0400

    premerge

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,10 +1,5 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-/**
- * @description Custom toggle hook that also sets and returns the height of a ref object
- * @param {array} initialValues [0] is boolean toggleValue, [1] is a ref object
- * @returns {array}
- */
 const useToggleHeight = initialValues => {
 	const [toggleValue, setValue] = useState(initialValues[0]);
 	const [componentHeight, setHeight] = useState(0);
