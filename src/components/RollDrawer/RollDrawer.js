4671fe7351c9a7ec2e9c08746321ab437e5c50a0
Elliott <elliottohj44@gmail.com>
2021-09-14 10:07:26 -0400

    lots of tinkering with styles of drawer UI components and behavior

--- src/components/RollDrawer/RollDrawer.js
+++ src/components/RollDrawer/RollDrawer.js
@@ -11,20 +11,39 @@ import './RollDrawer.css'
 const RollDrawer = () => {
   const heightRef = React.useRef(null)
   const [isExpanded, height, toggleExpand] = useToggleHeight([false, heightRef])
+  const [value, setValue] = React.useState('')
+
+  const handleDiceClick = (e, title) => {
+    e.preventDefault()
+    if (!value.trim()) {
+      setValue(title)
+    } else {
+      setValue(prev => prev + ` + ${title}`)
+    }
+  }
 
   const postResult = e => {
     e.preventDefault()
-    console.log('submitted')
+    console.log('value: ', value)
+    setValue('')
   }
 
   let currentHeight = isExpanded ? height : 0
 
   return (
-    <div className="drawer-container">
+    <div className={`drawer-container ${!isExpanded && 'drawer-container-open'}`}>
       <div className="drawer-header">
-        <button className="drawer-btn" onClick={e => toggleExpand(e)}>
-          open/close
-        </button>
+        {!isExpanded ? (
+          <button className="drawer-btn closed" onClick={e => toggleExpand(e)}>
+            <span className="sr-only">.</span>
+            <span className="arrowhead">⮝</span>
+          </button>
+        ) : (
+          <button className="drawer-btn opened" onClick={e => toggleExpand(e)}>
+            <span className="">Dice Roller</span>
+            <span className="arrowhead">⮟</span>
+          </button>
+        )}
       </div>
       <div
         className="drawer-body-container"
@@ -35,12 +54,20 @@ const RollDrawer = () => {
         <div className="drawer-body" ref={heightRef} aria-expanded={isExpanded} aria-hidden={!isExpanded}>
           <div className="results-container"></div>
           <div className="input-container">
-            <div className="dice-list">
-              <DiceList />
-            </div>
-            <form onSubmit={e => postResult(e)}></form>
-            <input type="text" />
-            <button className="submit-roll"></button>
+            <form onSubmit={e => postResult(e)}>
+              <div className="dice-list">
+                <Dice dice={d4} size={24} title={'d4'} onClick={handleDiceClick} />
+                <Dice dice={d6} size={24} title={'d6'} onClick={handleDiceClick} />
+                <Dice dice={d8} size={24} title={'d8'} onClick={handleDiceClick} />
+                <Dice dice={d10} size={24} title={'d10'} onClick={handleDiceClick} />
+                <Dice dice={d12} size={24} title={'d12'} onClick={handleDiceClick} />
+                <Dice dice={d20} size={24} title={'d20'} onClick={handleDiceClick} />
+              </div>
+              <div className="inputs-subcontainer">
+                <input className="drawer-input" type="text" value={value} onChange={e => setValue(e.target.value)} />
+                <button className="submit-btn">roll</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -48,23 +75,11 @@ const RollDrawer = () => {
   )
 }
 
-const Dice = ({ dice, size, title }) => {
+const Dice = ({ dice, size, title, onClick }) => {
   return (
-    <button title={title}>
+    <button title={title} className="dice-btn" onClick={e => onClick(e, title)}>
       <img src={dice} height={size} width={size} alt={`a${title === 'd8' && 'n'} ${title[1]}-sided dice`} />
     </button>
-  )
-}
-const DiceList = () => {
-  return (
-    <div className="dice-list">
-      <Dice dice={d4} size={32} title={'d4'} />
-      <Dice dice={d6} size={32} title={'d6'} />
-      <Dice dice={d8} size={32} title={'d8'} />
-      <Dice dice={d10} size={32} title={'d10'} />
-      <Dice dice={d12} size={32} title={'d12'} />
-      <Dice dice={d20} size={32} title={'d20'} />
-    </div>
   )
 }
 
